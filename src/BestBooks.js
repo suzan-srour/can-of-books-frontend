608152cd60b45dd3bdcbe6da7d7029277563bf42
suzan-srour <sroursuzan0@gmail.com>
2022-09-06 03:36:30 +0300

    front

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,5 +1,6 @@
 import React from 'react'; 
 import Carousel from 'react-bootstrap/Carousel'
+import Button from "react-bootstrap/Button";
 import axios from 'axios';
 class BestBooks extends React.Component {
   constructor(props) {
@@ -13,23 +14,81 @@ class BestBooks extends React.Component {
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
   componentDidMount = () => {
     axios
-      .get("http://localhost:3000/books")
+      .get("http://localhost:3001/books")
       .then(result => {
         console.log(result.data);
         this.setState({
           books: result.data
         })
-      }
+      })
+      .catch(err=>{
+        console.log(err);
+      })
+  } 
 
-      )
+
+  addBook = (event) =>{
+    event.preventDefault();
+
+    const obj = {
+      title : event.target.title.value,
+      description: event.target.description.value,
+      status :this.state.status,
+     
+    };
+    console.log(obj);
+    axios
+    .post(`http://localhost:3001/addbook`, obj)
+    .then(result =>{
+      this.setState({
+        books : result.data
+      })
+    })
+    .catch(err=>{
+      console.log(err);
+    })
   }
-  render() {
 
-    /* TODO: render all the books in a Carousel */
+  deleteBook = (id) => {
+    axios
+      .delete(`https://localhost:3001/deleteBook${id}`)
+      .then((result) => {
+        this.setState({
+          books: result.data,
+        });
+
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+  render() 
+  {
 
     return (
       <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+        <h2>My Essential Lifelong Learning  &amp; Formation Shelf</h2>
+           
+        <form onSubmit={this.props.addBook}>
+          <input type="text" name="title" placeholder='title' />
+          <input type="text" name="describtion" placeholder='describtion' />
+          <input type="text" name="status" placeholder='status' />
+
+          <button type='submit'>Add a book!</button>
+
+        </form>
 
 
         {this.state.books.length > 0 ? (
@@ -46,6 +105,11 @@ class BestBooks extends React.Component {
                   <Carousel.Caption>
                     <h3> title {item.title}</h3>
                     <p>description: {item.description}</p>
+                    <Button variant="light" 
+                          onClick={() => this.deleteBook(item._id)}
+                        >
+                          Delete This Book!
+                        </Button>
                   </Carousel.Caption>
                 </Carousel.Item>
 
